d29bfbaa7b3a6cb0297c972751597ebb384f5250
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2026-03-14 00:45:55 +0300

    header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,46 @@
+import { useState, useEffect } from "react";
 import css from "./Header.module.css";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const closeMenu = () => setOpen(false);
+
+  useEffect(() => {
+    const escClose = (e) => {
+      if (e.key === "Escape") closeMenu();
+    };
+
+    document.addEventListener("keydown", escClose);
+    return () => document.removeEventListener("keydown", escClose);
+  }, []);
+
   return (
-    <header className={css.header}>
-      <div className={css.logo}>LearnLingo</div>
+    <>
+      <header className={css.header}>
+        <div className={css.logo}>LearnLingo</div>
 
-      <nav className={css.nav}>
-        <a href="#">Home</a>
-        <a href="#">Teachers</a>
-      </nav>
+        <nav className={`${css.nav} ${open ? css.open : ""}`}>
+          <a href="#">Home</a>
+          <a href="#">Teachers</a>
 
-      <div className={css.actions}>
-        <button className={css.login}>Log in</button>
-        <button className={css.register}>Registration</button>
-      </div>
-    </header>
+          <div className={css.mobileActions}>
+            <button className={css.login}>Log in</button>
+            <button className={css.register}>Registration</button>
+          </div>
+        </nav>
+
+        <div className={css.actions}>
+          <button className={css.login}>Log in</button>
+          <button className={css.register}>Registration</button>
+
+          <button className={css.hamburger} onClick={() => setOpen(!open)}>
+            ☰
+          </button>
+        </div>
+      </header>
+
+      {open && <div className={css.backdrop} onClick={closeMenu}></div>}
+    </>
   );
 }
