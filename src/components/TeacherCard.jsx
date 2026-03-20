60a26b79b9580c8c12e99e18a57a787d03f22ef3
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2026-03-20 20:32:47 +0300

    teacherCard

--- src/components/TeacherCard.jsx
+++ src/components/TeacherCard.jsx
@@ -1,22 +1,22 @@
 import css from "./TeacherCard.module.css";
 
-export default function TeacherCard() {
+export default function TeacherCard({ teacher }) {
   return (
     <div className={css.card}>
       <div className={css.top}>
         <div className={css.avatar}></div>
 
         <div className={css.info}>
-          <p className={css.name}>John Doe</p>
-          <p className={css.lang}>English</p>
+          <p className={css.name}>
+            {teacher.name} {teacher.surname}
+          </p>
+          <p className={css.lang}>{teacher.languages.join(", ")}</p>
         </div>
 
-        <div className={css.price}>30$ / hour</div>
+        <div className={css.price}>{teacher.price_per_hour}$ / hour</div>
       </div>
 
-      <p className={css.desc}>
-        Experienced teacher with 5 years of teaching practice.
-      </p>
+      <p className={css.desc}>{teacher.experience}</p>
 
       <button className={css.readMore}>Read more</button>
     </div>
--- src/pages/TeachersPage.jsx
+++ src/pages/TeachersPage.jsx
@@ -1,16 +1,34 @@
 import css from "./TeachersPage.module.css";
 import TeacherCard from "../components/TeacherCard";
+
 export default function TeachersPage() {
+  const teachers = [
+    {
+      name: "John",
+      surname: "Doe",
+      languages: ["English"],
+      price_per_hour: 30,
+      experience: "5 years experience",
+    },
+    {
+      name: "Anna",
+      surname: "Smith",
+      languages: ["Spanish", "English"],
+      price_per_hour: 25,
+      experience: "3 years experience",
+    },
+  ];
+
   return (
     <section className={css.container}>
       <div className={css.filters}>Filters will be here</div>
 
-      <div className={css.list}>Teachers list</div>
       <div className={css.list}>
-        <TeacherCard />
-        <TeacherCard />
-        <TeacherCard />
+        {teachers.map((teacher, i) => (
+          <TeacherCard key={i} teacher={teacher} />
+        ))}
       </div>
+
       <button className={css.loadMore}>Load more</button>
     </section>
   );
